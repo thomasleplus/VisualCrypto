bc6798f7c438caf0ef6ef8c1e557faa79fae4c72
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-09-05 17:04:56 -0700

    Test

---
+++ src/test/java/org/leplus/keygen/TestMain.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.leplus.keygen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author thomas
+ *
+ */
+class TestMain {
+
+    @Test
+    void test() throws IOException {
+        Main.main(new String[] { "test" });
+    }
+
+}
